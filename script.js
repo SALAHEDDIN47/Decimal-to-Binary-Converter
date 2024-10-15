f724ebd50bf156032e315d26c22363b5e703c389
SALAHEDDIN47 <salaheddinejalloul538@gmail.com>
2024-10-15 12:05:56 +0100

    adding script

--- script.js
+++ script.js
@@ -13,7 +13,10 @@ const animationData = [
   },
   {
     inputVal: 1,
-    addElDelay: 2000
+    addElDelay: 2000,
+    msg: "decimalToBinary(1) returns '1' (base case) and gives that value to the stack below. Then it pops off the stack.",
+    showMsgDelay: 5000,
+    removeElDelay: 10000,
   }
 ];
 
@@ -29,7 +32,13 @@ const showAnimation = () => {
   result.innerText = "Call Stack Animation";
 
   animationData.forEach((obj) => {
-    setTimeOut( () => {} , obj.addElDelay );
+    setTimeout(() => {
+      animationContainer.innerHTML += `
+        <p id="${obj.inputVal}" class="animation-frame">
+          decimalToBinary(${obj.inputVal})
+        </p>
+      `;
+    }, obj.addElDelay);
   });
 };
 
